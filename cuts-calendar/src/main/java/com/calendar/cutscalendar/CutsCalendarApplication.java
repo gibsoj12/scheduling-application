07f3618109c7e4677b159dec831f55bed39de493
Jeff Gibson <gibsoj12@mcmaster.ca>
2019-12-22 13:07:31 -0500

    First commit

---
+++ cuts-calendar/src/main/java/com/calendar/cutscalendar/CutsCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.calendar.cutscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutsCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutsCalendarApplication.class, args);
+	}
+
+}
---
+++ cuts-calendar/src/test/java/com/calendar/cutscalendar/CutsCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendar.cutscalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutsCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
