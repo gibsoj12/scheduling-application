cc815ad2b760d28cf04c4b05e38cb1fb7ff0d5f9
root <root@DESKTOP-KTJLULT.localdomain>
2019-12-23 13:20:32 -0500

    Now asks user for their name and greets them.

--- cuts-calendar/src/main/java/com/calendar/cutscalendar/AppController.java
+++ cuts-calendar/src/main/java/com/calendar/cutscalendar/AppController.java
@@ -1,14 +1,26 @@
 package com.calendar.cutscalendar;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AppController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/")
     public String welcome() {
         System.out.println("AppController->welcome()");
-        return "classpath:resources/templates/index";
+        return "form";
     }
 }
