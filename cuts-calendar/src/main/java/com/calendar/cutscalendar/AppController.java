5dc10ec3fc0db860c8fbd17069b5ca14bb916a54
root <root@DESKTOP-KTJLULT.localdomain>
2019-12-23 15:18:05 -0500

    playing with dynamic views.

--- cuts-calendar/src/main/java/com/calendar/cutscalendar/AppController.java
+++ cuts-calendar/src/main/java/com/calendar/cutscalendar/AppController.java
@@ -1,26 +1,43 @@
 package com.calendar.cutscalendar;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AppController {
 
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) { //Model passes data between a controller and a view
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("/")
     public String welcome() {
         System.out.println("AppController->welcome()");
         return "form";
+    }
+
+    @GetMapping("available-dates")
+    public String displayDates(Model model) {
+        List<String> dates = new ArrayList<>(); //Need to figure out how to get these dates from the google calendar
+        dates.add("January 1");
+        dates.add("January 2");
+        dates.add("January 3");
+        dates.add("January 5");
+        model.addAttribute("dates", dates);
+        return "dates-list";
     }
 }
